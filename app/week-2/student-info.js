440fe1195993d7957b69e30e3a27a0029b2dd21d
OOP Project <davontemc.suf@gmail.com>
2025-01-16 11:58:37 -0700

    Added page.js and student-info.js
     page.js and student-info.js were created and added to week-2 folder

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+      <div>
+        <h1>Davonte McLean</h1>
+        <Link href="/https://github.com/DavonteMc/cprg306-assignments">GitHub Repository</Link>
+      </div>
+    );
+  }
