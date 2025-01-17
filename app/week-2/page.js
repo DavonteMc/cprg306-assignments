35d0c198cef086151ec53a50fb188a044bd9af8f
OOP Project <davontemc.suf@gmail.com>
2025-01-17 13:45:01 -0700

    Added Home page and Week-2 page

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,14 @@
+import StudentInfo from "./student-info";
+import Link from "next/link";
+
 export default function Page() {
-    return (
-      <main>
-        <h1>Shopping List</h1>
-      </main>
-    );
-  }
+  return (
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <StudentInfo />
+      <p>
+        <Link href="week-2">Home Page</Link>
+      </p>
+    </main>
+  );
+}
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,10 +1,12 @@
 import Link from "next/link";
 
 export default function StudentInfo() {
-    return (
-      <div>
-        <h1>Davonte McLean</h1>
-        <Link href="/https://github.com/DavonteMc/cprg306-assignments">GitHub Repository</Link>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Davonte McLean</h1>
+      <Link href="/https://github.com/DavonteMc/cprg306-assignments">
+        GitHub Repository
+      </Link>
+    </div>
+  );
+}
