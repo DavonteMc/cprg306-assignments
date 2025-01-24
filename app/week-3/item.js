a9b5d6e2eb5bcac69c2e34935b69747c82537821
OOP Project <davontemc.suf@gmail.com>
2025-01-24 13:32:58 -0700

    Made Minor Adjustments

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,4 +1,6 @@
 export default function Item({ name, quantity, category }) {
+    // let { name, quantity, category } = props
+
   return (
     <li className="text-xl font-bold bg-slate-900 p-2 my-4 w-15 m-10 ">
       {name}
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,6 +1,7 @@
 import ItemList from "./item-list"
 
-export default function Home() {
+export default function Page() {
+
     return (
       <main className="text-white bg-slate-950 ">
         <h1 className="text-3xl font-bold p-2">Shopping List</h1>
